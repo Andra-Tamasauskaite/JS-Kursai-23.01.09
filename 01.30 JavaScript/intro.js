46b81ec67b813fdbd7d303da1158bb6c9e8e7bfb
Andra-Tamasauskaite <andra.tamasauskaite@gmail.com>
2023-02-01 16:22:55 +0200

    Užduotis Nr.5

--- 01.30 JavaScript/intro.js
+++ 01.30 JavaScript/intro.js
@@ -3,9 +3,11 @@ let actName = "Jennifer";
 let actLast = "Aniston";
 
 if (actName.length > actLast.length) {
-  console.log(actLast);
+  console.log(`Trumpesnis kintamasis yra pavardė - ${actLast}.`);
+} else if (actName.length === actLast.length) {
+  console.log("Abu kintamieji yra vienodo ilgio.");
 } else {
-  console.log(actName);
+  console.log(`Trumpesnis kintamasis yra vardas - ${actName}.`);
 }
 
 // Task 2
@@ -23,8 +25,14 @@ console.log(result);
 //Task 3
 let nameA = "Harry";
 let nameB = "Potter";
-let combN = nameA.slice(2, 5) + nameB.slice(3, 6);
-
+//let combN = nameA.slice(2, 5) + nameB.slice(3, 6);
+let combN =
+  nameA[nameA.length - 3] +
+  nameA[nameA.length - 2] +
+  nameA[nameA.length - 1] +
+  nameB[nameB.length - 3] +
+  nameB[nameB.length - 2] +
+  nameB[nameB.length - 1];
 console.log(combN);
 
 //Task 4
@@ -46,3 +54,50 @@ let random3 = rand(0, 2);
 let random4 = rand(0, 2);
 
 console.log(random1, random2, random3, random4);
+
+let nuliai = 0;
+let vienetai = 0;
+let dvejatai = 0;
+
+if (random1 === 0) {
+  nuliai++;
+}
+if (random2 === 0) {
+  nuliai++;
+}
+if (random3 === 0) {
+  nuliai++;
+}
+if (random4 === 0) {
+  nuliai++;
+}
+
+if (random1 === 1) {
+  vienetai++;
+}
+if (random2 === 1) {
+  vienetai++;
+}
+if (random3 === 1) {
+  vienetai++;
+}
+if (random4 === 1) {
+  vienetai++;
+}
+
+if (random1 === 2) {
+  dvejatai++;
+}
+if (random2 === 2) {
+  dvejatai++;
+}
+if (random3 === 2) {
+  dvejatai++;
+}
+if (random4 === 2) {
+  dvejatai++;
+}
+
+console.log(
+  `Nulių yra: ${nuliai}, vienetų yra: ${vienetai}, dvejatų yra: ${dvejatai}.`
+);
